1db31cfd849734aaf8ee636f8585d03e3d795955
Lukas Bombach <lukas.bombach@googlemail.com>
2018-09-20 17:26:29 +0200

    enabled html webpack plugin for the dev server

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,19 +1,22 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const WebpackBar = require('webpackbar');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const baseConfig = require('./webpack.config.base');
-const { appClientJs, isProd } = require('../utils/paths');
+const { appClientJs, appHtml, isProd } = require('../utils/paths');
 
 const devPlugins = [
 	new VueSSRClientPlugin(),
-	new WebpackBar({ name: 'Client', color: 'green' })
+	new WebpackBar({ name: 'Client', color: 'green' }),
+	new HtmlWebpackPlugin({ template: appHtml, minify: { removeComments: false } })
 ];
 
 const prodPlugins = [
 	new UglifyJsPlugin({ sourceMap: false }),
 	new webpack.optimize.ModuleConcatenationPlugin(),
+	new HtmlWebpackPlugin({ template: appHtml, minify: { removeComments: false } })
 ];
 
 const config = merge(baseConfig, {
