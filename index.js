48c6f3f344e5233af469c0e713814c9c8aecc6a4
Lukas Bombach <lukas.bombach@googlemail.com>
2018-09-21 11:13:44 +0200

    exporting config in npm package

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const DevServer = require('./lib/server.dev');
 const ProdServer = require('./lib/server.prod');
 const Compiler = require('./lib/compiler');
 const middleware = require('./lib/middleware');
+const client = require('./build/webpack.config.client');
+const server = require('./build/webpack.config.server');
+const devServer = require('./build/webpack.config.server.dev');
 
 module.exports = {
   DevServer,
   ProdServer,
   Compiler,
-  middleware
+  middleware,
+  config: {
+    client,
+    server,
+    devServer,
+  }
 };
