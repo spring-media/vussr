7d17d319039f08ad744aefe5822968f0c1817e76
Lukas Bombach <lukas.bombach@googlemail.com>
2018-09-25 08:07:51 +0200

    bin.test.js

--- test/bin.test.js
+++ test/bin.test.js
@@ -1,1 +1,2 @@
-test('placeholder', () => {})
+// todo we should have tests for bin checking if error handling works
+test.skip('we should have tests for bin checking if error handling works', () => {})
