d7d2ef8aa12799eb4468b444df8b87c1ec384c2e
Lukas Bombach <lukas.bombach@googlemail.com>
2018-09-26 11:15:39 +0200

    checking if client js is accessible in prod server

--- test/server.prod.test.js
+++ test/server.prod.test.js
@@ -12,12 +12,21 @@ test('it runs on port 8080', async () => {
   server.close();
 });
 
-test('serves the app', async () => {
+test('serves the app\'s html', async () => {
   const server = await new ProdServer().listen();
   const response = await request(server.express).get('/');
   const expectedResponseText = expect.stringMatching(/^<!DOCTYPE html>\s*<html>\s*<head>\s*<meta charset="utf-8">\s*<meta http-equiv="X-UA-Compatible" content="IE=edge">\s*<meta name="viewport" content="width=device-width,initial-scale=1">\s*<link rel="preload" href="\/assets\/client\.[0-9a-f]+\.js" as="script"><style data-vue-ssr-id=".+?">\s*p\[data-v-[0-9]+\] \{\s*font-size: 2em;\s*text-align: center;\s*\}\s*<\/style><\/head>\s*<body><p id="#app" data-server-rendered="true" data-v-[0-9]+>Hello World!<\/p><script src="\/assets\/client\.[0-9a-f]+\.js" defer><\/script><script type="text\/javascript" src="\/assets\/client\.[0-9a-f]+\.js"><\/script><\/body>\s*<\/html>\n$/);
   expect(response.statusCode).toBe(200);
   expect(response.text).toEqual(expectedResponseText);
+  server.close();
+});
+
+test('serves the client js', async () => {
+  const server = await new ProdServer().listen();
+  const htmlResponse = await request(server.express).get('/');
+  const clientJsPath = htmlResponse.text.match(/\/assets\/client\.[0-9a-f]+\.js/);
+  const clientJsresponse = await request(server.express).get(clientJsPath);
+  expect(clientJsresponse.statusCode).toBe(200);
   server.close();
 });
 
