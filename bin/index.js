d14332827d1d6086b6e00bdecec6bae9231a67cb
Lukas Bombach <lukas.bombach@googlemail.com>
2018-12-08 12:12:37 +0100

    added graceful shutdown

--- bin/index.js
+++ bin/index.js
@@ -8,8 +8,14 @@ const Compiler = require('../lib/compiler');
 const logger = require('../lib/logger');
 const { printConfigHelp } = require('./utils');
 
+let server = null;
+
 ['unhandledRejection', 'uncaughtException'].forEach(event => {
   process.on(event, err => logger.error(err));
+});
+
+['SIGTERM'].forEach(event => {
+  process.on(event, () => server && server.close().then(() => process.exit(0)));
 });
 
 program
@@ -29,7 +35,7 @@ program
   .option('-c, --config <path>', 'provide a config file')
   .option('-e, --extend <path>', 'provide a config file to extend default config')
   .description('Starts a formerly created build with the production server')
-  .action(options => new ProdServer(options).listen())
+  .action(options => server = new ProdServer(options).listen())
   .on('--help', printConfigHelp);
 
 program
@@ -37,7 +43,7 @@ program
   .option('-c, --config <path>', 'provide a config file')
   .option('-e, --extend <path>', 'provide a config file to extend default config')
   .description('Serves the app with hot reloading for development')
-  .action(options => new DevServer(options).listen())
+  .action(options => server = new DevServer(options).listen())
   .on('--help', printConfigHelp);
 
 program
