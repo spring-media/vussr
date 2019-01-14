6847cf320e9297c90edafc02ec6fab9bf55f355c
Lukas Bombach <lukas.bombach@googlemail.com>
2019-01-14 09:45:48 +0100

    moved default config

---
+++ udssr.config.default.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+  entryClient: resolveApp('entry.client.js'),
+  entryServer: resolveApp('entry.server.js'),
+  outputPath: resolveApp('dist'),
+  assetsPath: resolveApp('dist/assets'),
+  filename: '[name].[chunkhash].js',
+  middleware: { before: [], after: [] },
+  copy: [],
+  server: defaultConfig => defaultConfig,
+  client: defaultConfig => defaultConfig,
+  devServer: defaultConfig => defaultConfig,
+};
