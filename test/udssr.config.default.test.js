177de4aace63c00c2c20e93cd010e0293430cf29
Lukas Bombach <lukas.bombach@googlemail.com>
2019-01-31 14:47:19 +0100

    wrote tests for default config

---
+++ test/udssr.config.default.test.js
@@ -0,0 +1,25 @@
+const defaultConfig = require('../src/udssr.config.default');
+
+test('matches the expected config', () => {
+  const expectedConfig = {
+    entryClient: expect.stringMatching(/src\/entry\.client\.js$/),
+    entryServer: expect.stringMatching(/src\/entry\.server\.js$/),
+    template: expect.stringMatching(/public\/index\.html$/),
+    outputPath: expect.stringMatching(/dist$/),
+    assetsPath: expect.stringMatching(/dist\/assets$/),
+    filename: '[name].[chunkhash].js',
+    middleware: { before: [], after: [] },
+    copy: [],
+    server: expect.any(Function),
+    client: expect.any(Function),
+    devServer: expect.any(Function),
+  };
+  expect(defaultConfig).toEqual(defaultConfig);
+});
+
+test('has proper deffault values for client, server, devServer', () => {
+  const value = { foo: 'bar' };
+  expect(defaultConfig.server(value)).toBe(value);
+  expect(defaultConfig.client(value)).toBe(value);
+  expect(defaultConfig.devServer(value)).toBe(value);
+});
