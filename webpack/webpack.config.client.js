b3fc5eed98e772de2d9f01692a1c332bcd6eddbb
Lukas Bombach <lukas.bombach@googlemail.com>
2019-02-26 14:29:20 +0100

    added default html and documentation

--- webpack/webpack.config.client.js
+++ webpack/webpack.config.client.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const WebpackBar = require('webpackbar');
@@ -5,8 +7,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
 const getBaseConfig = require('./webpack.config.base');
 const { isProd } = require('../src/utils/env');
+const defaultConfig = require('../src/vussr.config.default.js');
 
 module.exports = function getClientConfig(config) {
+  if (config.template === defaultConfig.template && !fs.existsSync(config.template)) {
+    config.template = path.resolve(__dirname, '../src/index.default.html');
+  }
+
+  if (!fs.existsSync(config.template)) {
+    throw new Error(`Cannot find template file ${config.template}`);
+  }
+
   const htmlWebpackPluginOptions = {
     template: config.template,
     inject: false,
