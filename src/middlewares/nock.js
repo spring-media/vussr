678a22ebbe09a017cbdacf38c8d159b21b70dc84
Lukas Bombach <lukas.bombach@googlemail.com>
2019-03-07 12:23:39 +0100

    bugfix for nock

--- src/middlewares/nock.js
+++ src/middlewares/nock.js
@@ -2,6 +2,6 @@ const { nockMiddleware, replayNocks } = require('express-nock');
 
 module.exports = function nock(mode, nockPath) {
   if (mode === 'record') return nockMiddleware({ nockPath });
-  if (mode === 'replay') return replayNocks({ nockPath });
+  if (mode === 'replay') replayNocks({ nockPath });
   return (req, res, next) => next();
 };
