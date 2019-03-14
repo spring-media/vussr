b9fe6277080f4a50cfdca61ec9cdd6e372c2a211
veit.bjarsch <veit.bjarsch@poweruplink.com>
2019-03-14 12:08:29 +0100

    escaped public path with quotation marks

--- webpack/webpack.config.devServer.js
+++ webpack/webpack.config.devServer.js
@@ -1,7 +1,13 @@
 module.exports = function getDevServerConfig(config) {
   return {
     contentBase: [config.outputPath, config.assetsPath],
-    publicPath: config.assetsPath,
+    /**
+     * Workaround because publicPath can't handle a path with spaces inside.
+     * This applies for unecaped as well escaped space characters.
+     * On the other hand all other configured path values for the webpack dev server
+     * can't handle an absoulte path escaped by quotation marks.
+     **/
+    publicPath: `"${config.assetsPath}"`,
     port: 8080,
     compress: false,
     overlay: true,
