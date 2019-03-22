df87cbdd65505895c614efd2bbf28ae45e884e0d
veit.bjarsch <veit.bjarsch@poweruplink.com>
2019-03-22 10:38:31 +0100

    updated public path to use a relative path, if the relative pah contains spaces an error will be thrown

--- webpack/webpack.config.devServer.js
+++ webpack/webpack.config.devServer.js
@@ -1,13 +1,22 @@
+const path = require('path');
+
+/**
+ * Transforming the public path to an relative path and check if it contains
+ * space symbols. This is a workaround because publicPath can't handle a path
+ * with spaces inside. This applies for unescaped as well escaped space characters.
+ * On the other hand all other configured path values for the webpack dev server
+ * should be absolute and can't handle an absoulte path escaped by quotation marks.
+ **/
+const exceptionMsg = val => `Spaces are not allowed inside the WebPack publicPath: ${val}`;
+const testSpaces = val => { if (/\s/.test(val)) { throw new Error(exceptionMsg(val))} return val };
+const transformSlashes = val => val.replace(/\\/, '/').replace(/^\/?/, '/').replace(/\/?$/, '/');
+const relativePath = val => transformSlashes(testSpaces(path.relative(process.cwd(), val)));
+
 module.exports = function getDevServerConfig(config) {
+  console.log(relativePath(config.assetsPath));
   return {
     contentBase: [config.outputPath, config.assetsPath],
-    /**
-     * Workaround because publicPath can't handle a path with spaces inside.
-     * This applies for unecaped as well escaped space characters.
-     * On the other hand all other configured path values for the webpack dev server
-     * can't handle an absoulte path escaped by quotation marks.
-     **/
-    publicPath: `"${config.assetsPath}"`,
+    publicPath: relativePath(config.assetsPath),
     port: 8080,
     compress: false,
     overlay: true,
