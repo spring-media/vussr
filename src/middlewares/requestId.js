1576fd2218a6b19c620dc10b071e601136ec67c6
veit.bjarsch <veit.bjarsch@poweruplink.com>
2019-05-21 16:08:16 +0200

    optimized request id class, added request Id to context

--- src/middlewares/requestId.js
+++ src/middlewares/requestId.js
@@ -1,34 +1,30 @@
-const cls = require('cls-hooked');
 const uuidv4 = require('uuid/v4');
 
-const nsid = uuidv4();
-const ns = cls.createNamespace(nsid);
+let reqId = null;
 
 /**
  * Express.js middleware
- * responsible for initializing the context for each request
- * Adds the request ID to the context
+ * responsible for initializing and saving the request id
+ * @return {function} express middleware
  **/
 const middleware = () => {
 	return (req, res, next) => {
-		ns.run(() => {
-			if (ns && ns.active) {
-				ns.set('reqId', uuidv4());
-			}
-			next();
-		});
-	}
-}
+		reqId = uuidv4();
+    console.log('Initialized: ', reqId);
+		next();
+	};
+};
 
 /**
- * Gets the request id from the context by key
- * Will return undefined if the context has not yet been initialized
- * for this request or if a value is not found for the specified key.
+ * Gets the request id
+ * Will return nothing if the context has not yet been initialized
+ * for this request or if there is no value present.
+ * @return {String} the current request id
  **/
-function get() {
-	if (ns && ns.active) {
-		return ns.get('reqId');
-	}
+function get () {
+  if (reqId) {
+    return reqId;
+  }
 }
 
 module.exports = { middleware, get };
--- src/middlewares/setContext.js
+++ src/middlewares/setContext.js
@@ -1,3 +1,5 @@
+const requestId = require('./requestId');
+
 module.exports = function setContext() {
   return (req, res, next) => {
     res.locals.context = {
@@ -5,6 +7,7 @@ module.exports = function setContext() {
       fullUrl: `${req.protocol}://${req.get('host')}${req.originalUrl}`,
       mobile: req.subdomains.indexOf('m') !== -1, // TODO this belongs to the app rather than to vussr
       desktop: req.subdomains.indexOf('m') === -1, // TODO this belongs to the app rather than to vussr
+      requestId: requestId.get(),
     };
     next();
   };
