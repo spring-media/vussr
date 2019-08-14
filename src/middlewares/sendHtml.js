f3cebc6dcfb885bcef9e05aca39f0cc9e60cb8a0
Robert Weber <closingtag@gmail.com>
2019-08-14 22:30:27 +0200

    Add content type for html (#56)
    * Add content type for html
    * Adjust test

--- src/middlewares/sendHtml.js
+++ src/middlewares/sendHtml.js
@@ -1,6 +1,7 @@
 module.exports = function sendHtml() {
   return (req, res, next) => {
     try {
+      res.setHeader('Content-Type', 'text/html');
       res.end(res.locals.body);
     } catch (err) {
       next(err);
