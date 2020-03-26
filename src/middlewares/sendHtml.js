17a383554cc243dbfd355b38bed5afc40a0c12a7
Michael Feit <michael.feit@gmx.de>
2020-03-26 11:16:31 +0100

    Added response header logger

--- src/middlewares/sendHtml.js
+++ src/middlewares/sendHtml.js
@@ -1,10 +1,12 @@
-var etag = require('etag');
+const etag = require('etag');
+const logger = require('../src/logger');
 
 module.exports = function sendHtml() {
   return (req, res, next) => {
     try {
       res.setHeader('Content-Type', 'text/html');
       res.setHeader('ETag', etag(res.locals.body));
+      logger.info(`ResponseHeaders => ${JSON.stringify(res.getHeaders())}`);
       res.end(res.locals.body);
     } catch (err) {
       next(err);
