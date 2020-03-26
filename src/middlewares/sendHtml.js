77618a7a32f5b446687078cdee7e2297c69e4027
Michael Feit <feitm@users.noreply.github.com>
2020-03-26 19:35:46 +0100

    feature/REDC-4038 set strong ETag header and add resposeHeader log (#67)
    * set strong etag header to response
    
    * set etag to dev
    
    * draft
    
    * create strong ETag based on Body Response
    
    * Added response header logger
    
    * fix path
    
    * bump version to 4.3.4

--- src/middlewares/sendHtml.js
+++ src/middlewares/sendHtml.js
@@ -1,7 +1,12 @@
+const etag = require('etag');
+const logger = require('../logger.js');
+
 module.exports = function sendHtml() {
   return (req, res, next) => {
     try {
       res.setHeader('Content-Type', 'text/html');
+      res.setHeader('ETag', etag(res.locals.body));
+      logger.info(`ResponseHeaders => ${JSON.stringify(res.getHeaders())}`);
       res.end(res.locals.body);
     } catch (err) {
       next(err);
