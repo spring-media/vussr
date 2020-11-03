a49e0f9da1725b9d68a41a05f8201f36d6408bc3
Jördis Schuller <joerdis.schuller@spring-media.de>
2020-11-03 21:40:56 +0100

    change config for clean webpack plugin

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const SpritePlugin = require('svg-sprite-loader/plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 const { isProd } = require('../src/utils/env');
@@ -153,7 +153,7 @@ module.exports = function getBaseConfig(config) {
     plugins: [
       new VueLoaderPlugin(),
       new SpritePlugin({ plainSprite: true }),
-      new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [config.outputPath] }),
+      new CleanWebpackPlugin(),
     ],
   };
 };
